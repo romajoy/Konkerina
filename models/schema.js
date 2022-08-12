546cfef6223d59d250cc7a7620f428ce3cb76c35
Joy Houser <joyhouser@Joys-MacBook-Air.local>
2022-08-12 15:27:43 -0500

    updated index ejs

--- models/schema.js
+++ models/schema.js
@@ -4,25 +4,20 @@ const mongoose = require('mongoose')
 
 // Match Schema
 const needleSchema = new mongoose.Schema({
+    // type: {type: String, required: true}, // needle or hook, if hook show size, brand, material?
     size: {type: String, required: true}, // ex. US 6
     brand: {type: String, required: true}, // ex. Chicagoo
-    type: {type: String, required: true}, // ex. Circular
+    style: {type: String, required: true}, // ex. Circular
     length: {type: Number}, // ex. 16"
     material: {type: String}, // ex. metal, dropdown menu
     point: {type: String}, // ex. lace point, dropdown menu
-    setComplete: {type: Boolean}, // yes or no
+    setComplete: {type: Boolean}, // yes or no // only show if DPN or Straight
     inUse: {type: Boolean}, // yes or no
-    project: {type: String}, // ex. Polyrhythm
+    project: {type: String}, // ex. Polyrhythm // only show if inUse is Y
     replace: {type: Boolean}, // want to replace soon
     favorite: {type: Boolean}, // true or false
-    notes: {type: String} // ex. not great for socks, best for wool
+    misc: {type: String} // ex. not great for socks, best for wool
 })
-
-// needs replacing soon
-// searchable
-// interchangable cable compatability
-
-
 
 const Needle = mongoose.model('Needle', needleSchema)
 module.exports = Needle
